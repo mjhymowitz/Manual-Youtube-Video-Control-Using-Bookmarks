7aab2458aff942ee01bf6dba9a618b3d65cbbb8c
Mark Hymowitz <mark.hymowitz@gmail.com>
2024-06-18 20:42:50 -0600

    Add guard and update go to timestamp

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 javascript:(
   function(){
     var video = document.getElementsByClassName("html5-main-video")[0];
+    if(video == null){
+      return nil;
+    }
+
     video.currentTime -= 5;
   }
 )();
@@ -10,6 +14,10 @@ javascript:(
 javascript:(
   function(){
     var video = document.getElementsByClassName("html5-main-video")[0];
+    if(video == null){
+      return nil;
+    }
+
     video.currentTime += 5;
   }
 )();
@@ -17,8 +25,12 @@ javascript:(
 // Speedup video speed by user input
 javascript:(
   function(){
-    var speed = prompt("What speed would you like this to play at?");
     var video = document.getElementsByClassName("html5-main-video")[0];
+    if(video == null){
+      return nil;
+    }
+
+    var speed = prompt("What speed would you like this to play at?");
     if (speed!==null){
       video.playbackRate = speed;
     }
@@ -28,8 +40,12 @@ javascript:(
 // Go to timestamp
 javascript:(
   function(){
-    var timestamp = prompt("Go to timestame (seconds)?");
     var video = document.getElementsByClassName("html5-main-video")[0];
+    if(video == null){
+      return nil;
+    }
+
+    var timestamp = prompt("Go to timestame ("+video.duration.toFixed(0)+")?");
     if (timestamp!==null){
       video.currentTime = timestamp;
     }
