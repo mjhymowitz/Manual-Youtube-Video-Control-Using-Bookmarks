5df075838fc76fb7efcd314cfeb4c9610116a696
Mark Hymowitz <mark.hymowitz@gmail.com>
2024-06-30 19:20:15 -0600

    Update for multiple video tags

--- main.js
+++ main.js
@@ -1,53 +1,64 @@
 // Go back 5 seconds
 javascript:(
   function(){
-    var video = document.getElementsByClassName("html5-main-video")[0];
+    var video = document.getElementsByClassName("html5-main-video");
     if(video == null){
       return nil;
     }
 
-    video.currentTime -= 5;
+    video.forEach(element => {
+      element.currentTime -= 5;
+    });
   }
 )();
 
 // Go forward 5 seconds
 javascript:(
   function(){
-    var video = document.getElementsByClassName("html5-main-video")[0];
+    var video = document.getElementsByClassName("html5-main-video");
     if(video == null){
       return nil;
     }
-
-    video.currentTime += 5;
+    video.forEach(element => {
+      element.currentTime += 5;
+    });
   }
 )();
 
 // Speedup video speed by user input
 javascript:(
   function(){
-    var video = document.getElementsByClassName("html5-main-video")[0];
+    var video = document.getElementsByClassName("html5-main-video");
     if(video == null){
       return nil;
     }
 
     var speed = prompt("What speed would you like this to play at?");
-    if (speed!==null){
-      video.playbackRate = speed;
+    if (speed == null){
+      return nil;
     }
+
+    video.forEach(element => {
+      element.playbackRate = speed;
+    });
   }
 )();
 
 // Go to timestamp
 javascript:(
   function(){
-    var video = document.getElementsByClassName("html5-main-video")[0];
+    var video = document.getElementsByClassName("html5-main-video");
     if(video == null){
       return nil;
     }
 
     var timestamp = prompt("Go to timestame ("+video.duration.toFixed(0)+")?");
-    if (timestamp!==null){
-      video.currentTime = timestamp;
+    if (timestamp == null){
+      return nil;
     }
+
+    video.forEach(element => {
+      element.currentTime = timestamp;
+    });
   }
 )();
